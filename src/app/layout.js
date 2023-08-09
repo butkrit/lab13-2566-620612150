cca4aaf42138b7142d932d232437d52c81051998
butkrit <yokart.butter@gmail.com>
2023-08-09 14:34:21 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Thanakrit Yokart"
+            studentId="620612150"
           />
         </div>
       </body>
